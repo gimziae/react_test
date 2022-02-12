a1f2533d5d9603dcdf87ca3ab528d327ddad5ab3
gimziae <gimziae@gmail.com>
2022-02-12 16:33:47 +0900

    HasgRouter 변경

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 // 라우터 설정
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
     
     {/* 라우터 태그로 감싸준다 */}
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
 
   </React.StrictMode>,
   document.getElementById('root')
